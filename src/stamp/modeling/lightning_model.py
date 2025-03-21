ef98b2e136329c21c315342670c7677b5d211d08
Georg Wölflein <georgw7777@gmail.com>
2025-03-21 17:41:10 +0100

    reattac)

--- src/stamp/modeling/lightning_model.py
+++ src/stamp/modeling/lightning_model.py
@@ -131,7 +131,7 @@ class LitVisionTransformer(lightning.LightningModule):
 
         self.log(
             f"{step_name}_loss",
-            loss.detach(),
+            loss,
             on_step=False,
             on_epoch=True,
             prog_bar=True,
@@ -140,7 +140,7 @@ class LitVisionTransformer(lightning.LightningModule):
 
         if step_name == "validation":
             # TODO this is a bit ugly, we'd like to have `_step` without special cases
-            self.valid_auroc(logits.detach(), targets.long().argmax(-1).detach())
+            self.valid_auroc(logits, targets.long().argmax(-1))
             self.log(
                 f"{step_name}_auroc",
                 self.valid_auroc,

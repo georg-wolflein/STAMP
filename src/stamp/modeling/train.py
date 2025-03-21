c5bb67d3370e6d650ea0df96aa8da149c7f73e93
Georg Wölflein <georgw7777@gmail.com>
2025-03-21 17:46:39 +0100

    2048

--- src/stamp/modeling/train.py
+++ src/stamp/modeling/train.py
@@ -244,7 +244,7 @@ def setup_model_for_training(
     del categories  # Let's not accidentally reuse the original categories
     valid_dl, _ = dataloader_from_patient_data(
         patient_data=[patient_to_data[patient] for patient in valid_patients],
-        bag_size=1024,  # Fix colab out of memory error
+        bag_size=2048,  # Fix colab out of memory error
         categories=train_categories,
         batch_size=1,
         shuffle=False,
